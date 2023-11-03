dcf9e88a446f6e9c35bae06ce7c64b0394421552
paradoxo <vinicius_baggio@hotmail.com>
2023-11-03 19:01:28 -0300

    feat: Action clear, del, digits, dot and operator

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import {
   IonPage,
   IonRow,
 } from "@ionic/react";
-import { backspaceOutline } from "ionicons/icons";
+import { atCircleOutline, backspaceOutline } from "ionicons/icons";
 import { useReducer } from "react";
 import IonColButton from "../components/IonColButton";
 import "./Home.css";
@@ -20,33 +20,121 @@ export type Action = {
 type State = {
   current: string;
   previous: string;
+  expression: string;
 };
+
+const OPERATORS = { "+": "+", "-": "-", "÷": "/", "✕": "*" };
 
 export const ACTIONS = {
-  DELETE: "delete",
   CLEAR: "clear",
+  DELETE: "delete",
   DIGIT: "digit",
+  DOT: "dot",
   OPERATOR: "operator",
+  EVALUATE: "evaluate",
+  PARENTHESIS: "parenthesis",
 };
 
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case ACTIONS.DELETE:
+function reducerEvaluate(state: State, action: Action): State {}
+
+function reducerOperator(state: State, action: Action): State {
+  const lastChar = state.current.at(-1);
+
+  if (!lastChar) {
+    if (action.value === "-") {
+      return {
+        ...state,
+        current: state.current + action.value,
+        expression: state.expression + OPERATORS[action.value],
+      };
+    } else {
+      return {
+        ...state,
+      };
+    }
+  }
+
+  if (!(lastChar in OPERATORS)) {
+    return {
+      ...state,
+      current: state.current + action.value,
+      expression: state.expression + OPERATORS[action.value],
+    };
+  }
+
+  if (action.value !== "-") {
+    if (state.current.length === 1) {
+      return {
+        ...state,
+        current: "",
+        expression: "",
+      };
+    } else if (lastChar === "-" && state.current.at(-2) in OPERATORS) {
       return {
         ...state,
         current: state.current.slice(0, -1),
+        expression: state.expression.slice(0, -1),
       };
+    } else {
+      return {
+        ...state,
+        current: state.current.slice(0, -1) + action.value,
+        expression: state.expression.slice(0, -1) + OPERATORS[action.value],
+      };
+    }
+  }
+
+  if (lastChar !== "-") {
+    return {
+      ...state,
+      current: state.current + action.value,
+      expression: state.expression + OPERATORS[action.value],
+    };
+  }
+
+  return {
+    ...state,
+  };
+}
+
+function reducerDot(state: State, action: Action): State {
+  if (state.current.includes(".")) {
+    return {
+      ...state,
+    };
+  }
+
+  return {
+    ...state,
+    current: state.current + action.value,
+  };
+}
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
     case ACTIONS.CLEAR:
       return {
         current: "",
         previous: "",
+        expression: "",
+      };
+    case ACTIONS.DELETE:
+      return {
+        ...state,
+        current: state.current.slice(0, -1),
       };
     case ACTIONS.DIGIT:
       return {
         ...state,
         current: state.current + action.value,
       };
+    case ACTIONS.DOT:
+      return reducerDot(state, action);
     case ACTIONS.OPERATOR:
+      return reducerOperator(state, action);
+    case ACTIONS.EVALUATE:
+      return reducerEvaluate(state, action);
+    case ACTIONS.PARENTHESIS:
       break;
   }
 
@@ -59,6 +147,7 @@ const Home: React.FC = () => {
   const [state, dispatch] = useReducer(reducer, {
     current: "",
     previous: "",
+    expression: "",
   });
 
   function handleDelete() {
@@ -67,6 +156,10 @@ const Home: React.FC = () => {
 
   function handleClear() {
     dispatch({ type: ACTIONS.CLEAR, value: "" });
+  }
+
+  function handleOperator(value: string) {
+    dispatch({ type: ACTIONS.OPERATOR, value });
   }
 
   return (
@@ -83,35 +176,57 @@ const Home: React.FC = () => {
             <IonCol className="button-col" onClick={(_) => handleClear()}>
               AC
             </IonCol>
-            <IonCol className="button-col">(</IonCol>
-            <IonCol className="button-col">)</IonCol>
-            <IonCol className="button-col">÷</IonCol>
+            <IonCol className="button-col" onClick={(_) => handleOperator("(")}>
+              (
+            </IonCol>
+            <IonCol className="button-col" onClick={(_) => handleOperator(")")}>
+              )
+            </IonCol>
+            <IonCol className="button-col" onClick={(_) => handleOperator("÷")}>
+              ÷
+            </IonCol>
           </IonRow>
           <IonRow>
             <IonColButton text="7" dispatch={dispatch} />
             <IonColButton text="8" dispatch={dispatch} />
             <IonColButton text="9" dispatch={dispatch} />
-            <IonCol className="button-col">✕</IonCol>
+            <IonCol className="button-col" onClick={(_) => handleOperator("✕")}>
+              ✕
+            </IonCol>
           </IonRow>
           <IonRow>
             <IonColButton text="4" dispatch={dispatch} />
             <IonColButton text="5" dispatch={dispatch} />
             <IonColButton text="6" dispatch={dispatch} />
-            <IonCol className="button-col">-</IonCol>
+            <IonCol className="button-col" onClick={(_) => handleOperator("-")}>
+              -
+            </IonCol>
           </IonRow>
           <IonRow>
             <IonColButton text="1" dispatch={dispatch} />
             <IonColButton text="2" dispatch={dispatch} />
             <IonColButton text="3" dispatch={dispatch} />
-            <IonCol className="button-col">+</IonCol>
+            <IonCol className="button-col" onClick={(_) => handleOperator("+")}>
+              +
+            </IonCol>
           </IonRow>
           <IonRow>
             <IonColButton text="0" dispatch={dispatch} />
-            <IonCol className="button-col">.</IonCol>
+            <IonCol
+              className="button-col"
+              onClick={(_) => dispatch({ type: ACTIONS.DOT, value: "." })}
+            >
+              .
+            </IonCol>
             <IonCol className="button-col" onClick={(_) => handleDelete()}>
               <IonIcon icon={backspaceOutline}></IonIcon>
             </IonCol>
-            <IonCol className="button-col">=</IonCol>
+            <IonCol
+              className="button-col"
+              onClick={(_) => dispatch({ type: ACTIONS.EVALUATE, value: "" })}
+            >
+              =
+            </IonCol>
           </IonRow>
         </IonGrid>
       </IonContent>
